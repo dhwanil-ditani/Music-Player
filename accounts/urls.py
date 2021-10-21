fd0c415f1b4a889deeae3a7e761100f1548ffc1d
dhwanil-ditani <ditanidhwanil@gmail.com>
2021-10-21 21:05:17 +0530

    update user authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import SignupView
-
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('register/', SignupView.as_view(), name='Register')
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+    path('register/', SignupView.as_view(), name='register')
 ]
